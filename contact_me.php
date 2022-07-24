82e81d715e0c73b439b7b7bae10c15044b8a5842
rwebdesign2022 <109924910+rwebdesign2022@users.noreply.github.com>
2022-07-24 16:27:42 -0700

    Update contact_me.php

--- contact_me.php
+++ contact_me.php
@@ -17,7 +17,7 @@
     $to = 'faizalroy2019@gmail.com';
     $email_subject = "Website Contact Form:  $name";
     $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nSituation:\n$situation\n\nLegal Aid:\n$legalaid";
-    $headers = "From: noreply@protect-my-child.ca\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+    $headers = "From: noreply@ontario-family-law.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
     $headers .= "Reply-To: $email_address";
     mail($to,$email_subject,$email_body,$headers);
     return true;
